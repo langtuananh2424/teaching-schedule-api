b25b2a037fcf8630cafddcb802be16dfe3d934f0
Tyr <langtuananh2424@gmail.com>
2025-09-24 11:45:50 +0700

    modified AttendanceRepository

--- src/main/java/com/thuyloiuni/teaching_schedule_api/repository/AttendanceRepository.java
+++ src/main/java/com/thuyloiuni/teaching_schedule_api/repository/AttendanceRepository.java
@@ -1,5 +1,21 @@
 package com.thuyloiuni.teaching_schedule_api.repository;
 
-public class AttendanceRepository {
-    
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thuyloiuni.teaching_schedule_api.entity.Attendance;
+
+@Repository
+public interface AttendanceRepository extends JpaRepository<Attendance, Integer> {
+
+    List<Attendance> findBySchedule_SessionId(Integer sessionId);
+
+    List<Attendance> findByStudent_StudentId(Integer studentId);
+
+    Optional<Attendance> findBySchedule_SessionIdAndStudent_StudentId(Integer sessionId, Integer studentId);
+
+    List<Attendance> findBySchedule_SessionIdAndIsPresent(Integer sessionId, Boolean isPresent);
 }
