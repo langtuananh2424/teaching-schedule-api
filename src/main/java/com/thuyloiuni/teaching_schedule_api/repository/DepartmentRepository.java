e1e3a8f7212812bc170cc6beb073f63c294e10ab
Tyr <langtuananh2424@gmail.com>
2025-09-24 11:15:09 +0700

    modified DepartmentRepository

--- src/main/java/com/thuyloiuni/teaching_schedule_api/repository/DepartmentRepository.java
+++ src/main/java/com/thuyloiuni/teaching_schedule_api/repository/DepartmentRepository.java
@@ -1,5 +1,14 @@
 package com.thuyloiuni.teaching_schedule_api.repository;
 
-public class DepartmentRepository {
-    
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thuyloiuni.teaching_schedule_api.entity.Department;
+
+@Repository
+
+public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+    Optional<Department> findByDepartName(String departName);
 }
