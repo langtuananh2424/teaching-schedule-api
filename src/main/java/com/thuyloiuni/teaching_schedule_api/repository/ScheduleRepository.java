d632e42ba5c37f4c85237ac364b5fae746ad8b16
Tyr <langtuananh2424@gmail.com>
2025-09-24 11:39:17 +0700

    modified ScheduleRepository

--- src/main/java/com/thuyloiuni/teaching_schedule_api/repository/ScheduleRepository.java
+++ src/main/java/com/thuyloiuni/teaching_schedule_api/repository/ScheduleRepository.java
@@ -1,5 +1,37 @@
 package com.thuyloiuni.teaching_schedule_api.repository;
 
-public class ScheduleRepository {
-    
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thuyloiuni.teaching_schedule_api.entity.Schedule;
+import com.thuyloiuni.teaching_schedule_api.entity.enums.ScheduleStatus;
+
+@Repository
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    List<Schedule> findByAssignment_AssignmentId(Integer assignmentId);
+
+    List<Schedule> findByAssignment_Lecturer_LecturerId(Integer lecturerId);
+
+    List<Schedule> findBySessionDate(LocalDate sessionDate);
+
+    List<Schedule> findBySessionDateBetween(LocalDate startDate, LocalDate endDate);
+
+    List<Schedule> findByAssignment_Lecturer_LecturerIdAndSessionDateBetween(Integer lecturerId, LocalDate startDate, LocalDate endDate);
+
+    List<Schedule> findByStatus(ScheduleStatus status);
+
+    // @Query("SELECT s FROM Schedule s WHERE s.assignment.lecturer.lecturerId = :lecturerId " +
+    //        "AND s.sessionDate = :date " +
+    //        "AND s.startPeriod = :startPeriod")
+    // Optional<Schedule> findByLecturerAndDateAndStartPeriod(
+    //         @Param("lecturerId") Integer lecturerId,
+    //         @Param("date") LocalDate date,
+    //         @Param("startPeriod") Integer startPeriod
+    // );
+
+    List<Schedule> findByAssignment_StudentClass_ClassIdAndSessionDate(Integer classId, LocalDate sessionDate);
 }
