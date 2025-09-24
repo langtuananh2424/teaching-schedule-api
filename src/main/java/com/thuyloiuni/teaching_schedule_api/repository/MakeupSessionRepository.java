5786f774c1ee5ce5ffe49b6192768f8467446e59
Tyr <langtuananh2424@gmail.com>
2025-09-24 11:53:22 +0700

    modified MakeupSessionRepository

--- src/main/java/com/thuyloiuni/teaching_schedule_api/repository/MakeupSessionRepository.java
+++ src/main/java/com/thuyloiuni/teaching_schedule_api/repository/MakeupSessionRepository.java
@@ -1,5 +1,20 @@
 package com.thuyloiuni.teaching_schedule_api.repository;
 
-public class MakeupSessionRepository {
-    
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thuyloiuni.teaching_schedule_api.entity.MakeupSession;
+import com.thuyloiuni.teaching_schedule_api.entity.enums.ApprovalStatus;
+
+@Repository
+
+public interface MakeupSessionRepository extends JpaRepository<MakeupSession, Integer> {
+    Optional<MakeupSession> findByOriginalAbsentSession_SessionId(Integer absentSessionId);
+
+    List<MakeupSession> findByApprover_LecturerId(Integer approverId);
+
+    List<MakeupSession> findByApprovalStatus(ApprovalStatus status);
 }
