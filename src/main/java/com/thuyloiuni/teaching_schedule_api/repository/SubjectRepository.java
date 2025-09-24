46df9a337083e499e8965b81545cf31c78f28bb2
Tyr <langtuananh2424@gmail.com>
2025-09-24 11:20:31 +0700

    modified SubjectRepository

--- src/main/java/com/thuyloiuni/teaching_schedule_api/repository/SubjectRepository.java
+++ src/main/java/com/thuyloiuni/teaching_schedule_api/repository/SubjectRepository.java
@@ -1,5 +1,19 @@
 package com.thuyloiuni.teaching_schedule_api.repository;
 
-public class SubjectRepository {
-    
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thuyloiuni.teaching_schedule_api.entity.Subject;
+
+@Repository
+
+public interface SubjectRepository extends JpaRepository<Subject, Integer> {
+    Optional<Subject> findBySubjectCode(String subjectCode);
+
+    List<Subject> findByDepartment_DepartId(Integer departId);
+
+    List<Subject> findByCredits(Integer credits);
 }
