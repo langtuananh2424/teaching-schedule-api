2dd10b862fbf67f57129cfbd6b4c376036cf1de7
Tyr <langtuananh2424@gmail.com>
2025-09-24 11:23:03 +0700

    modified StudentClassRepository

--- src/main/java/com/thuyloiuni/teaching_schedule_api/repository/StudentClassRepository.java
+++ src/main/java/com/thuyloiuni/teaching_schedule_api/repository/StudentClassRepository.java
@@ -1,5 +1,17 @@
 package com.thuyloiuni.teaching_schedule_api.repository;
 
-public class StudentClassRepository {
-    
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thuyloiuni.teaching_schedule_api.entity.StudentClass;
+
+@Repository
+
+public interface StudentClassRepository extends JpaRepository<StudentClass, Integer> {
+    Optional<StudentClass> findByClassCode(String classCode);
+
+    List<StudentClass> findBySemester(String semester);
 }
