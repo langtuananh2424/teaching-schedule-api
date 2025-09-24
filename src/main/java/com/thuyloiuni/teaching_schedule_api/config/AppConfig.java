6633f4773555f1a2dd281fbff4e78045f184fecf
Tyr <langtuananh2424@gmail.com>
2025-09-24 12:34:26 +0700

    Adding modelMapper

--- src/main/java/com/thuyloiuni/teaching_schedule_api/config/AppConfig.java
+++ src/main/java/com/thuyloiuni/teaching_schedule_api/config/AppConfig.java
@@ -1,5 +1,18 @@
 package com.thuyloiuni.teaching_schedule_api.config;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
 public class AppConfig {
     
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper new modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
 }
