3c465af99abe4b9f70452639d0f5a0a69200318a
Tyr <langtuananh2424@gmail.com>
2025-09-24 11:27:15 +0700

    modified AssignmentRepository

--- src/main/java/com/thuyloiuni/teaching_schedule_api/repository/AssignmentRepository.java
+++ src/main/java/com/thuyloiuni/teaching_schedule_api/repository/AssignmentRepository.java
@@ -1,5 +1,20 @@
 package com.thuyloiuni.teaching_schedule_api.repository;
 
-public class AssignmentRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thuyloiuni.teaching_schedule_api.entity.Assignment;
+
+@Repository
+
+public interface AssignmentRepository extends JpaRepository<Assignment, Integer> {
+    List<Assignment> findByLecturer_LecturerId(Integer lecturerId);
+
+    List<Assignment> findBySubject_SubjectId(Integer subjectId);
+
+    List<Assignment> findByClass_ClassId(Integer classId);
+
+    List<Assignment> findByLecturer_LecturerIdAndSubject_SubjectId(Integer lecturerId, Integer subjectId);
 }
