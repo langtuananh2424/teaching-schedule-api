b1a0f96faf4000a5da8790154c2c25fd8a779223
Tyr <langtuananh2424@gmail.com>
2025-09-24 11:48:22 +0700

    modified AbsenceRequestRepository

--- src/main/java/com/thuyloiuni/teaching_schedule_api/repository/AbsenceRequestRepository.java
+++ src/main/java/com/thuyloiuni/teaching_schedule_api/repository/AbsenceRequestRepository.java
@@ -1,5 +1,21 @@
 package com.thuyloiuni.teaching_schedule_api.repository;
 
-public class AbsenceRequestRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thuyloiuni.teaching_schedule_api.entity.AbsenceRequest;
+import com.thuyloiuni.teaching_schedule_api.entity.enums.ApprovalStatus;
+
+@Repository
+
+public interface AbsenceRequestRepository extends JpaRepository<AbsenceRequest, Integer> {
+
+    List<AbsenceRequest> findBySchedule_SessionId(Integer sessionId);
+
+    List<AbsenceRequest> findByApprovalStatus(ApprovalStatus status);
+
+    List<AbsenceRequest> findByApprover_LecturerId(Integer approverId);
+
 }
